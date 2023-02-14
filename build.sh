18464f0a4bd9818b6a971691c53758a6b04cba06
AoThen <32790381+AoThen@users.noreply.github.com>
2023-02-14 16:54:54 +0800

    补齐

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ sudo -E apt-get update
 sudo -E apt-get install git  asciidoc bash bc binutils bzip2 fastjar flex gawk gcc genisoimage gettext git intltool jikespg libgtk2.0-dev libncurses5-dev libssl-dev make mercurial patch perl-modules python2.7-dev rsync ruby sdcc subversion unzip util-linux wget xsltproc zlib1g-dev zlib1g-dev -y
 
 git config --global user.email "${EMAIL}"
-git config --global user.name "aa"
+git config --global user.name "AoThen"
 [ -n "${PASSWORD}" ] && git config --global user.password "${PASSWORD}"
 
 mkdir -p  ${WORKDIR}/buildsource
@@ -63,6 +63,9 @@ esac
 
 cd openwrt-sdk
 sed -i "1i\src-link githubaction ${WORKDIR}/buildsource" feeds.conf.default
+
+#OpenClash
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 ls -l
 cat feeds.conf.default
